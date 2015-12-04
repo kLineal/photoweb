b81f62b6e432dd19362184466ee162a34b34da59
enriqueLarraia <elarraia@gmail.com>
2015-12-04 17:53:48 +0000

    match_all keywords in a view

--- app/controllers/admin/collection_views_controller.rb
+++ app/controllers/admin/collection_views_controller.rb
@@ -89,7 +89,7 @@ def show
  keywords = []
  @this_view.collection_tags.each {  |tag| keywords<< tag.keyword }
  
- @photos = CollectionPhoto.tagged_with(keywords, :on => :keywords, :any => true)
+ @photos = CollectionPhoto.tagged_with(keywords, :on => :keywords, :match_all => true)
   
   #render plain: @photos.each {|photo| puts photo.name}
   #render plain: keywords.inspect
