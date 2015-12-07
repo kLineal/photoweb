a6a650dafb9ddd5381c2d6d40f97bd5a9a172662
enriqueLarraia <elarraia@gmail.com>
2015-12-07 16:52:52 +0000

    view types constant as class method

--- app/models/collection_view.rb
+++ app/models/collection_view.rb
@@ -2,11 +2,20 @@ class CollectionView < ActiveRecord::Base
 
 has_and_belongs_to_many :collection_tags, join_table: "collection_tags_views"
 
-def self.view_types
+###
+# CLASS METHODS
+###
+def CollectionView.view_types
 
   ['cover', 'partial']
 
 end
+
+
+###
+# INSTANCE METHODS
+###
+
 
 # get keyword attribute of associated collection_tag records
 def keywords
