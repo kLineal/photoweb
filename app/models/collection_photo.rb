b4e9b808d5d90fa2f8951d92ce17fde8bf70992e
enriqueLarraia <elarraia@gmail.com>
2015-12-09 14:39:24 +0000

    global constant for path to images

--- app/models/collection_photo.rb
+++ app/models/collection_photo.rb
@@ -3,4 +3,9 @@ class CollectionPhoto < ActiveRecord::Base
 # context maps to attribute 'keyword' of collection_tag table 
 acts_as_taggable_on :keywords
 
+def path_to_image
+  # constant defined in config/initializers/constants.rb
+  path_to_image = "#{COLLECTION_FOLDER_PATH}/" << self.name  
+end
+
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,2 @@
+# path to directory containing collection photos
+COLLECTION_FOLDER_PATH = "/images/collection"
